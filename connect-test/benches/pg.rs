9cc9ae452ea118176ac1debc7bb66673419f58fb
Julius de Bruijn <julius+github@nauk.io>
2021-10-11 10:21:54 +0200

    Databases

--- connect-test/benches/pg.rs
+++ connect-test/benches/pg.rs
@@ -1,9 +1,17 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use tokio::runtime::Runtime;
-use tokio_postgres::{Client, NoTls};
+use tokio_postgres::{config::SslMode, Client, Config, NoTls};
 
-async fn connect(conn_str: &str) -> anyhow::Result<Client> {
-    let (client, connection) = tokio_postgres::connect(conn_str, NoTls).await?;
+async fn connect(port: u16) -> anyhow::Result<Client> {
+    let mut config = Config::new();
+
+    config.user("postgres");
+    config.password("prisma");
+    config.host("127.0.0.1");
+    config.port(port);
+    config.dbname("postgres");
+
+    let (client, connection) = config.connect(NoTls).await.unwrap();
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
@@ -14,9 +22,9 @@ async fn connect(conn_str: &str) -> anyhow::Result<Client> {
     Ok(client)
 }
 
-fn connect_test(rt: &Runtime, conn_str: &str) {
+fn full_connect_test(rt: &Runtime, port: u16) {
     rt.block_on(async {
-        connect(conn_str).await.unwrap();
+        connect(port).await.unwrap();
     })
 }
 
@@ -29,23 +37,16 @@ fn _query_test(rt: &Runtime, client: &Client, query: &str) {
 fn criterion_benchmark(c: &mut Criterion) {
     let rt = Runtime::new().unwrap();
 
-    let pg13 = "postgresql://postgres:prisma@127.0.0.1:5435/postgres";
-    let pg14 = "postgresql://postgres:prisma@127.0.0.1:5437/postgres";
+    let pg13 = 5435;
+    let pg14 = 5437;
 
-    c.bench_function("pg13 connect", |b| b.iter(|| connect_test(&rt, black_box(pg13))));
-    c.bench_function("pg14 connect", |b| b.iter(|| connect_test(&rt, black_box(pg14))));
-
-    /*
-    c.bench_function("pg13 select", |b| {
-        let client = rt.block_on(async { connect(pg13).await.unwrap() });
-        b.iter(|| query_test(&rt, &client, black_box("SELECT 1")));
+    c.bench_function("pg14 full connect", |b| {
+        b.iter(|| full_connect_test(&rt, black_box(pg14)))
     });
 
-    c.bench_function("pg14 select", |b| {
-        let client = rt.block_on(async { connect(pg14).await.unwrap() });
-        b.iter(|| query_test(&rt, &client, black_box("SELECT 1")));
+    c.bench_function("pg13 full connect", |b| {
+        b.iter(|| full_connect_test(&rt, black_box(pg13)))
     });
-    */
 }
 
 criterion_group!(benches, criterion_benchmark);
